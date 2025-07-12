ff4f0be67f4383e6e8b3bfea1b688edc86ea76af
guangwit9 <guangwit@gmail.com>
2025-07-12 16:10:16 +0800

    Update ArgoSBAUTO.sh

--- ArgoSBAUTO.sh
+++ ArgoSBAUTO.sh
@@ -3,6 +3,7 @@
 # 参数检查（nix 为可选）
 : "${uuid:?必须提供 uuid}"
 : "${vmpt:?必须提供 vmpt}"
+: "${argo:?必须提供 argo}"
 : "${agn:?必须提供 agn}"
 : "${agk:?必须提供 agk}"
 : "${TOKEN:?必须提供 TOKEN}"
@@ -13,7 +14,7 @@
 echo "[1/3] 安装 ArgosB..."
 
 # 构建安装命令，只有在 nix 被设置时才加入
-install_cmd="uuid='${uuid}' vmpt='${vmpt}' agn='${agn}' agk='${agk}'"
+install_cmd="uuid='${uuid}' vmpt='${vmpt}' argo='${argo}' agn='${agn}' agk='${agk}'"
 [ -n "${nix}" ] && install_cmd="nix='${nix}' $install_cmd"
 
 sudo bash -c "$install_cmd bash <(curl -Ls https://raw.githubusercontent.com/yonggekkk/argosb/main/argosb.sh)"
